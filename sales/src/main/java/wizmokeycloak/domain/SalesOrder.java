362ae97f835b11915d1ff783b7bc646624e2440a
kibumpark0405 <130956306+kibumpark0405@users.noreply.github.com>
2023-09-05 14:55:38 +0900

    commit

---
+++ sales/src/main/java/wizmokeycloak/domain/SalesOrder.java
@@ -0,0 +1,54 @@
+package wizmokeycloak.domain;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+import lombok.Data;
+import wizmokeycloak.SalesApplication;
+import wizmokeycloak.domain.SalesOrderCreated;
+import wizmokeycloak.domain.SalesOrderDeleted;
+import wizmokeycloak.domain.SalesOrderUpdated;
+
+@Entity
+@Table(name = "SalesOrder_table")
+@Data
+//<<< DDD / Aggregate Root
+public class SalesOrder {
+
+    @Id
+    private String salesOrderNumber;
+
+    private String salesPerson;
+
+    private SalesType salesType;
+
+    @Embedded
+    private CompanyId companyId;
+
+    @Embedded
+    private SalesItem salesItem;
+
+    @PostPersist
+    public void onPostPersist() {
+        SalesOrderCreated salesOrderCreated = new SalesOrderCreated(this);
+        salesOrderCreated.publishAfterCommit();
+
+        SalesOrderUpdated salesOrderUpdated = new SalesOrderUpdated(this);
+        salesOrderUpdated.publishAfterCommit();
+
+        SalesOrderDeleted salesOrderDeleted = new SalesOrderDeleted(this);
+        salesOrderDeleted.publishAfterCommit();
+    }
+
+    @PreRemove
+    public void onPreRemove() {}
+
+    public static SalesOrderRepository repository() {
+        SalesOrderRepository salesOrderRepository = SalesApplication.applicationContext.getBean(
+            SalesOrderRepository.class
+        );
+        return salesOrderRepository;
+    }
+}
+//>>> DDD / Aggregate Root
---
+++ sales/src/main/java/wizmokeycloak/infra/SalesOrderHateoasProcessor.java
@@ -0,0 +1,17 @@
+package wizmokeycloak.infra;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.RepresentationModelProcessor;
+import org.springframework.stereotype.Component;
+import wizmokeycloak.domain.*;
+
+@Component
+public class SalesOrderHateoasProcessor
+    implements RepresentationModelProcessor<EntityModel<SalesOrder>> {
+
+    @Override
+    public EntityModel<SalesOrder> process(EntityModel<SalesOrder> model) {
+        return model;
+    }
+}
